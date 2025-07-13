b921aa8c7d8231b08c02cfd65b57c0b3f6bf876d
David <david.vega@puntosingular.com>
2025-07-12 22:58:44 -0500

    Merge pull request #20 from extradajanet/f1-Vega-y-Francisco
    F1 vega y francisco

---
+++ sistema-creditos-backend/SistemaCreditosComplementarios.Core/Models/CarreraModel/CarreraModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCreditosComplementarios.Core.Models.CarreraModel
+{
+    public class CarreraModels
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
