8f10e834b913b86d6ccf99d1b25e897d7c4741cc
David <david.vega@puntosingular.com>
2025-07-12 23:06:17 -0500

    Reapply "Merge pull request #20 from extradajanet/f1-Vega-y-Francisco"
    This reverts commit 98ae93c7c54f58ce2bbfbdbb576e442a1e1b5d01.

---
+++ sistema-creditos-backend/SistemaCreditosComplementarios.Core/Settings/IdentityDataInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCreditosComplementarios.Core.Settings
+{
+    public static class IdentityDataInitializer
+    {
+        public static async Task CreateRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roleNames = { "Alumno", "Departamento", "Administrador" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+
+            }
+        }
+    }
+}
