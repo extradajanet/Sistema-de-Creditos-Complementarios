003d81e237bc4437602a892a26957add553f0af4
danacrdn <danacrdn@gmail.com>
2025-07-18 17:52:18 -0500

    Modificar scroll en CursosDisponibles

--- sistema-creditos-frontend/src/pages/alumno/CursosDisponibles.jsx
+++ sistema-creditos-frontend/src/pages/alumno/CursosDisponibles.jsx
@@ -133,7 +133,7 @@ useEffect(() => {
   );
 
   return (
-    <div className="flex flex-col gap-6 w-full">
+    <div className="flex flex-col gap-6 w-full h-screen">
       {/* Título */}
       <div className="flex justify-between items-center bg-gray-200 rounded-xl p-6">
         <h1 className="text-3xl font-bold  text-gray-900 custom-heading">
@@ -204,7 +204,7 @@ useEffect(() => {
       </div>
 
       {/* Lista de actividades */}
-      <div className="max-h-[500px] overflow-y-auto pr-2">
+      <div className="flex-1 overflow-y-auto pr-2 mb-8">
         <div className="grid grid-cols-1 sm:grid-cols-4 gap-9 ">
           {loading ? (
             <p className="text-center col-span-full mt-10 text-black-600">
