ee37adf232ee5dcb9c5f1849faa0a452c9eec0f4
extradajanet <168400503+extradajanet@users.noreply.github.com>
2025-07-20 22:15:37 -0500

    Update Login and Register
    Update labels for login and registration fields
    Add "Forgot Password" dialog

--- sistema-creditos-frontend/src/pages/Auth/Register.jsx
+++ sistema-creditos-frontend/src/pages/Auth/Register.jsx
@@ -19,7 +19,6 @@ function Register() {
   useEffect(() => {
     const fetchCareers = async () => {
       try {
-
         const res = await fetch("https://localhost:7238/api/Carrera/carreras");
 
         const data = await res.json();
@@ -42,26 +41,23 @@ function Register() {
     }
 
     try {
-      const response = await fetch(
-        "https://localhost:7238/api/Auth/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            nombre: name,
-            apellido: surnname,
-            numeroControl: numcontrol,
-            semestre: parseInt(semestre),
-            email,
-            password,
-            confirmPassword: confirmpassword,
-            carreraId: career?.id,
-            totalCreditos: 0,
-          }),
-        }
-      );
+      const response = await fetch("https://localhost:7238/api/Auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre: name,
+          apellido: surnname,
+          numeroControl: numcontrol,
+          semestre: parseInt(semestre),
+          email,
+          password,
+          confirmPassword: confirmpassword,
+          carreraId: career?.id,
+          totalCreditos: 0,
+        }),
+      });
 
       if (!response.ok) {
         throw new Error("Error al registrarse");
@@ -91,87 +87,116 @@ function Register() {
               className="flex flex-col items-center space-y-3"
               onSubmit={handleSubmit}
             >
-              <label htmlFor="name" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Nombre
-              </label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Nombre"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-md"
-              />
-              <label htmlFor="surnname" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Apellidos
-              </label>
-              <input
-                type="text"
-                value={surnname}
-                onChange={(e) => setSurname(e.target.value)}
-                placeholder="Apellidos"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
-              <label htmlFor="numcontrol" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Número de control
-              </label>
-              <input
-                type="text"
-                value={numcontrol}
-                onChange={(e) => setNumcontrol(e.target.value)}
-                placeholder="Número de Control"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
-              <label htmlFor="semestre" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Semestre
-              </label>
-              <input
-                type="number"
-                value={semestre}
-                onChange={(e) => setSemestre(e.target.value)}
-                placeholder="Semestre"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
-              <label htmlFor="email" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Correo electrónico
-              </label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Correo electrónico"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="name"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Nombre"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-md"
+                />
+              </div>
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="surnname"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Apellidos
+                </label>
+                <input
+                  type="text"
+                  value={surnname}
+                  onChange={(e) => setSurname(e.target.value)}
+                  placeholder="Apellidos"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="numcontrol"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Número de control
+                </label>
+                <input
+                  type="text"
+                  value={numcontrol}
+                  onChange={(e) => setNumcontrol(e.target.value)}
+                  placeholder="Número de Control"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="semestre"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Semestre
+                </label>
+                <input
+                  type="number"
+                  value={semestre}
+                  onChange={(e) => setSemestre(e.target.value)}
+                  placeholder="Semestre"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="email"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Correo electrónico
+                </label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Correo electrónico"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
+                />
+              </div>
 
               <CareerDropdown
                 careers={careers}
                 onChange={(val) => setCareer(val)}
               />
+              <div className="flex flex-col text-left">
+                <label
+                  htmlFor="password"
+                  className="mb-1 text-sm font-semibold text-[#001F54]"
+                >
+                  Contraseña
+                </label>
 
-              <label htmlFor="password" className="mb-1 text-sm font-semibold text-[#001F54]">
-                Contraseña
-              </label>
-
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Contraseña"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
-              <input
-                type="password"
-                value={confirmpassword}
-                onChange={(e) => setConfirmpassword(e.target.value)}
-                placeholder="Confirmar Contraseña"
-                required
-                className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
-              />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Contraseña"
+                  required
+                  className="w-[250px] px-4 py-2 mb-2 border border-[#001F54] rounded-lg"
+                />
+                <input
+                  type="password"
+                  value={confirmpassword}
+                  onChange={(e) => setConfirmpassword(e.target.value)}
+                  placeholder="Confirmar Contraseña"
+                  required
+                  className="w-[250px] px-4 py-2 border border-[#001F54] rounded-lg"
+                />
+              </div>
 
               {error && (
                 <p className="text-red-500 text-sm text-center">{error}</p>
